20f1d23f5ef3b5650b2a781781bff2385a6ae6de
nandukalidindi <nandukalidindi@gmail.com>
2017-03-25 21:23:51 -0400

    Update landing page to meet the assignment requirement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,84 +12,202 @@
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        .flexsearch--wrapper {
+          height: auto;
+          width: auto;
+          max-width: 100%;
+          overflow: hidden;
+          background: transparent;
+          margin: 0;
+          position: static;
+          }
 
-            .full-height {
-                height: 100vh;
-            }
+          .flexsearch--form {
+          overflow: hidden;
+          position: relative;
+          }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+          .flexsearch--input-wrapper {
+          padding: 0 30px 0 0;
+          /* Right padding for submit button width */
+          overflow: hidden;
+          }
 
-            .position-ref {
-                position: relative;
-            }
+          .flexsearch--input {
+          width: 100%;
+          }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+          /***********************
+          * Configurable Styles *
+          ***********************/
+          .flexsearch {
+            width: 50%;
+            /* Padding for other horizontal elements */
+          }
 
-            .content {
-                text-align: center;
-            }
+          .flexsearch--input {
+          -webkit-box-sizing: content-box;
+          -moz-box-sizing: content-box;
+          box-sizing: content-box;
+          height: 60px;
+          padding: 0 10px 0 10px;
+          border-color: #888;
+          border-radius: 35px;
+          /* (height/2) + border-width */
+          border-style: solid;
+          border-width: 5px;
+          margin-top: 15px;
+          color: #333;
+          font-family: 'Helvetica', sans-serif;
+          font-size: 26px;
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          }
 
-            .title {
-                font-size: 84px;
-            }
+          .flexsearch--submit {
+          position: absolute;
+          right: 0;
+          top: 0;
+          display: block;
+          width: 60px;
+          height: 60px;
+          padding: 0;
+          border: none;
+          margin-top: 20px;
+          /* margin-top + border-width */
+          margin-right: 5px;
+          /* border-width */
+          background: transparent;
+          color: #888;
+          font-family: 'Helvetica', sans-serif;
+          font-size: 40px;
+          line-height: 60px;
+          }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+          .flexsearch--input:focus {
+          outline: none;
+          border-color: #333;
+          }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+          .flexsearch--input:focus.flexsearch--submit {
+          color: #333;
+          }
+
+          .flexsearch--submit:hover {
+          color: #333;
+          cursor: pointer;
+          }
+
+          ::-webkit-input-placeholder {
+          color: #888;
+          }
+
+          input:-moz-placeholder {
+          color: #888;
+          }
+
+          .h1 {
+          float: left;
+          margin: 25px;
+          color: #333;
+          font-family: 'Helvetica', sans-serif;
+          font-size: 45px;
+          font-weight: bold;
+          line-height: 45px;
+          text-align: center;
+          }
+
+          .flex-box {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+
+
+          .submit-container {
+          	float: left;
+          	width: 60px;
+          	height: 32px;
+          	margin: 7px 0 0 7px;
+          	padding: 0;
+          	cursor: pointer;
+          	background: #e63c0d;
+          	border: 1px solid #d62700;
+          	-moz-border-radius: 6px;
+          	-webkit-border-radius: 6px;
+          	border-radius: 6px;
+          	background-image: -webkit-gradient(linear, left top, left bottom, from(#f55f15), to(#e63c0d));
+          	background-image: -webkit-linear-gradient(top, #f55f15, #e63c0d);
+          	background-image: -moz-linear-gradient(top, #f55f15, #e63c0d);
+          	background-image: -ms-linear-gradient(top, #f55f15, #e63c0d);
+          	background-image: -o-linear-gradient(top, #f55f15, #e63c0d);
+          	background-image: linear-gradient(top, #f55f15, #e63c0d);
+          	-moz-box-shadow: 0 1px 1px #fa8e1f inset, 0 1px 0 #fff;
+          	-webkit-box-shadow: 0 1px 1px #fa8e1f inset, 0 1px 0 #fff;
+          	box-shadow: 0 1px 1px #fa8e1f inset, 0 1px 0 #fff;
+          }
+
+          .submit-container:hover, .submit-container:focus {
+          	background-image: -webkit-gradient(linear, left top, left bottom, from(#e63c0d), to(#f55f15));
+          	background-image: -webkit-linear-gradient(top, #e63c0d, #f55f15);
+          	background-image: -moz-linear-gradient(top, #e63c0d, #f55f15);
+          	background-image: -ms-linear-gradient(top, #e63c0d, #f55f15);
+          	background-image: -o-linear-gradient(top, #e63c0d, #f55f15);
+          	background-image: linear-gradient(top, #e63c0d, #f55f15);
+          	-moz-box-shadow: 0 1px 1px #f36215 inset, 0 1px 0 #fff;
+          	-webkit-box-shadow: 0 1px 1px #f36215 inset, 0 1px 0 #fff;
+          	box-shadow: 0 1px 1px #f36215 inset, 0 1px 0 #fff;
+          }
+
+          .submit-container:active {
+          	outline: 0;
+          	-moz-box-shadow: 0 1px 6px #e4340b inset;
+          	-webkit-box-shadow: 0 1px 6px #e4340b inset;
+          	box-shadow: 0 1px 6px #e4340b inset;
+          }
+
+          .submit {
+          	float: left;
+          	width: 60px;
+          	height: 32px;
+          	margin: 0;
+          	padding: 0;
+          	cursor: pointer;
+          	border: 0px solid #000;
+          	background: url(images/magnifier.png) 20px 6px no-repeat;
+          	-moz-border-radius: 6px;
+          	-webkit-border-radius: 6px;
+          	border-radius: 6px;
+          }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+      <div class="flex-box">
+        <div class="h1"> MARKETPLACE </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div class="flexsearch">
+          <div class="flexsearch--wrapper">
+            <form class="flexsearch--form" action="#" method="post">
+              <div class="flexsearch--input-wrapper">
+                <input class="flexsearch--input" type="search" placeholder="Enter Customer name">
+              </div>
+            </form>
+          </div>
         </div>
+
+        <div class="flexsearch">
+          <div class="flexsearch--wrapper">
+            <form class="flexsearch--form" action="#" method="post">
+              <div class="flexsearch--input-wrapper">
+                <input class="flexsearch--input" type="search" placeholder="Product keyword ">
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div class="submit-container">
+				  <input type="submit" value="" class="submit" />
+			  </div>
+      </div>
     </body>
 </html>
